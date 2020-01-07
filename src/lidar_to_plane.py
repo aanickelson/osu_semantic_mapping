614d130c981bde62b5c7b7e60bfafbb82b17c404
aanickelson <nickelsa@oregonstate.edu>
2020-01-06 18:33:45 -0800

    Ok.... so here's the stitch:
    - PCL doesn't seem to work so well... I tested it on the dummy data (z = 1 for all except a few outliers) and it seemed to pick points at random and fit a plane to those.
    - The current challenge is that matplotlib hangs with large amounts of data (e.g. trying to plot 30k points). Need to downsample in order to plot.
    
    Ay.

--- src/lidar_to_plane.py
+++ src/lidar_to_plane.py
@@ -1,13 +1,17 @@
 
 # Code copied from https://stackoverflow.com/questions/39772424/how-to-effeciently-convert-ros-pointcloud2-to-pcl-point-cloud-and-visualize-it-i
 
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 import rospy
 import pcl
 from sensor_msgs.msg import PointCloud2
 import ros_numpy
 import numpy as np
 import sensor_msgs.point_cloud2 as pc2
-import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
@@ -15,6 +19,28 @@ class LidarData:
     def __init__(self):
         self.p = None
         self.it = 0
+
+    def callback(self, data):
+        pc = ros_numpy.numpify(data)
+
+        height = pc.shape[0]
+        width = pc.shape[1]
+        points = np.zeros((height * width, 3), dtype=np.float32)
+        points[:, 0] = np.resize(pc['x'], height * width)
+        points[:, 1] = np.resize(pc['y'], height * width)
+        points[:, 2] = np.resize(pc['z'], height * width)
+
+        p = pcl.PointCloud(np.array(points, dtype=np.float32))
+
+        x_range = [np.min(points[:, 0]), np.max(points[:, 0])]
+        y_range = [np.min(points[:, 1]), np.max(points[:, 1])]
+        z_range = [np.min(points[:, 2]), np.max(points[:, 2])]
+
+        if self.it == 10:
+            self.p = p
+            main(self.p, x_range, y_range, z_range, points)
+
+        self.it += 1
 
     def ros_to_pcl(self, ros_cloud):
         """ Converts a ROS PointCloud2 message to a pcl PointXYZRGB
@@ -33,7 +59,7 @@ class LidarData:
         z = []
 
         for data in pc2.read_points(ros_cloud, skip_nans=True):
-            # print("data", (data))
+            # print("data", (data[0]))
             points_list.append([data[0], data[1], data[2]])  #, data[3]])
             x.append(data[0])
             y.append(data[1])
@@ -42,18 +68,14 @@ class LidarData:
         pcl_data = pcl.PointCloud()
         pcl_data.from_list(points_list)
 
-        x_range = [np.min(x), np.max(x)]
-        y_range = [np.min(y), np.max(y)]
-        z_range = [np.min(z), np.max(z)]
-
-        if self.it == 0:
+        if self.it == 10:
             self.p = pcl_data
-            main(self.p, x_range, y_range, z_range)
+            main(self.p, x, y, z)
 
         self.it += 1
 
 
-def main(cloud, x_range, y_range, z_range):
+def main(cloud, x, y, z):
     print('Point cloud data: ' + str(cloud.size) + ' points')
     # for i in range(0, cloud.size):
     #     print('x: ' + str(cloud[i][0]) + ', y : ' +
@@ -76,14 +98,24 @@ def main(cloud, x_range, y_range, z_range):
         coefficients[1]) + ' ' + str(coefficients[2]) + ' ' + str(coefficients[3]))
 
     print('Model inliers: ' + str(len(indices)))
-    # for i in range(0, len(indices)):
-    #     print(str(indices[i]) + ', x: ' + str(cloud[indices[i]][0]) + ', y : ' +
-    #           str(cloud[indices[i]][1]) + ', z : ' + str(cloud[indices[i]][2]))
 
-    plot_plane(coefficients, x_range, y_range, z_range)
+    x_n = []
+    y_n = []
+    z_n = []
+    for i in range(0, len(indices)):
+        x_n.append(cloud[indices[i]][0])
+        y_n.append(cloud[indices[i]][1])
+        z_n.append(cloud[indices[i]][2])
+        # print(str(indices[i]) + ', x: ' + str(cloud[indices[i]][0]) + ', y : ' +
+        #       str(cloud[indices[i]][1]) + ', z : ' + str(cloud[indices[i]][2]))
+
+    plot_plane(coefficients, x, y, z)
 
 
-def plot_plane(coefficients, x_range, y_range, z_range):
+def plot_plane(coefficients, x, y, z):
+    x_range = [np.min(x), np.max(y)]
+    y_range = [np.min(y), np.max(y)]
+
     # PLANE -------------------------------
 
     # THIS IS A STRONG ASSUMPTION - assuming that the x and y coordinates are uniformly distributed
@@ -91,23 +123,19 @@ def plot_plane(coefficients, x_range, y_range, z_range):
     x_plane = np.arange(x_range[0], x_range[1], 0.01)
     y_plane = np.arange(y_range[0], y_range[1], 0.01)
 
-    X, Y = np.meshgrid(x_plane, y_plane)
-    # print(x_plane)
-    #
-    # x_plot, y_plot = np.meshgrid(x_plane, y_plane)
+    X, Y = np.meshgrid(x, y)
 
     a = coefficients[0]
     b = coefficients[1]
     c = coefficients[2]
     d = coefficients[3]
 
-    z_plot = (a * X + b * Y + d) / c
-    # print(z_plot)
+    z_plot = (-a * X - b * Y + d) / c
 
     fig = plt.figure()
     ax = fig.add_subplot(1, 2, 1, projection='3d')
-    # ax = fig.gca(projection='3d')
-    surf = ax.plot_surface(X, Y, z_plot, color='b')
+
+    surf = ax.scatter(x, y, z, color='b')
 
     # POINTS ---------------------------------
     ax2 = fig.add_subplot(1, 2, 2, projection='3d')
