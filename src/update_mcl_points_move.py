ed7c5f0eea2fce063cc4bdaf593e8bf6b7eae77f
Anna Nickelson <aanickelson@gmail.com>
2020-02-07 16:11:17 -0800

    Initial commit of mcl point translations with movement using only odom.

--- src/update_mcl_points_move.py
+++ src/update_mcl_points_move.py
@@ -9,31 +9,47 @@ from math import pi
 
 class OdomTransform:
     def __init__(self):
+        # Store the prior message (roll, pitch, yaw), current message, and difference between them
         self.last_msg = [0, 0, 0]
         self.this_msg = [0, 0, 0]
+        # Difference will be cumulative since the last time it was used, as the MCL updates and odom updates will
+        # likely be out of sync.
         self.diff = [0, 0, 0]
 
     def get_diff(self, msg):
-        self.last_msg = self.this_msg
-        (r, p, y) = tf.transformations.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
+        # Translate to euler coordinates
+        (r, p, y) = tf.transformations.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y,
+                                                              msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
+        # Update current odom reading
         self.this_msg = [r, p, y]
 
+        # Calculate difference between them
         self.diff += [self.this_msg[0] - self.last_msg[0],
                      self.this_msg[1] - self.last_msg[1],
                      self.this_msg[2] - self.last_msg[2]]
 
+        # Update current message to be the last message received
+        self.last_msg = self.this_msg
+
     def translate_points(self, points):
+        # Add a 0 to diff array as a placeholder for the probability so it can be easily subtracted
         diff = np.array(self.diff + [0])
+
+        # Loop through and translate to new position
         for i in range(len(points)):
             points[i] = np.subtract(points[i], diff)
 
-        # Reset the difference to zero
+        # Reset the difference to zero so it can start counting up again
         self.diff = [0, 0, 0]
+
+        return points
 
 
 if __name__ == "__main__":
     odom = OdomTransform()
     rospy.init_node('update_mcl_points_py', anonymous=True)
+
+    # Placeholder for testing
     points = np.random.rand(100, 4)
     for i in range(len(points)):
         points[i][1] *= 5
@@ -41,7 +57,5 @@ if __name__ == "__main__":
         points[i][2] *= 2 * pi
 
     rospy.Subscriber("/odom", Odometry, odom.get_diff)
-
-    odom.translate_points(points)
-
+    print(odom.translate_points(points))
     rospy.spin()
