b5a506377bba80031196561f0a613ea827f6f321
Anna Nickelson <aanickelson@gmail.com>
2020-05-13 11:57:49 -0700

    Got edge data node to work. Now publishes to a topic whenever the current node changes

--- src/interval_graph_planning/gather_edge_data.py
+++ src/interval_graph_planning/gather_edge_data.py
@@ -5,9 +5,9 @@ import rospy
 # Brings in the .action file and messages used by the move base action
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from hospital_graph_class import HospitalGraph
+from std_msgs.msg import String
 
 from two_rooms_v3_parameters import TwoRoomsParameters
-import interval_cust
 
 
 class RoboInfo:
@@ -16,19 +16,20 @@ class RoboInfo:
         self.prior_node = None
         self.current_node = None
         self.p = p
+
+        self.pub = rospy.Publisher('node_in_graph', String, queue_size=10)
         # self.building = HospitalGraph(p.num_rooms, p.num_halls, p.extra_doors,
         #                               p.hall_door_links, p.extra_door_hall_links, p.connected_halls)
 
     def set_current_pose(self, msg):
+        # Set the robot's current location and determine what node it is in
         self.current_pose = (msg.pose.pose.position.x, msg.pose.pose.position.y)
-        print(self.current_pose)
-        self.what_node()
-        print(self.current_node)
+        self.current_node = self.what_node()
 
+        # If the node changed, publish it to a topic
         if self.prior_node != self.current_node:
-            # TODO: Publish that the robot transitioned between nodes
-            # print(self.current_node)
-            pass
+            rospy.loginfo(self.current_node)
+            self.pub.publish(self.current_node)
 
     def what_node(self):
         """
@@ -48,7 +49,7 @@ class RoboInfo:
         for node in self.p.nodes_dict:
             if 'd' in node:
                 if self.current_pose[0] in self.p.nodes_dict[node][0] and self.current_pose[1] in self.p.nodes_dict[node][1]:
-                    self.current_node = node
+                    return node
 
         # Loop through a second time for all other areas
         for node in self.p.nodes_dict:
@@ -56,26 +57,18 @@ class RoboInfo:
                 continue
             else:
                 if self.current_pose[0] in self.p.nodes_dict[node][0] and self.current_pose[1] in self.p.nodes_dict[node][1]:
-                    self.current_node = node
+                    return node
 
         raise ValueError('Robot is out of this world')
 
 
 if __name__ == "__main__":
-    rospy.init_node('edge_loc_py')
+    while not rospy.is_shutdown():
+        rospy.init_node('node_in_graph_py')
 
-    r = RoboInfo(p=TwoRoomsParameters)
+        r = RoboInfo(p=TwoRoomsParameters)
 
-    amcl_sub = rospy.Subscriber('amcl_pose', PoseWithCovarianceStamped, r.set_current_pose)
+        amcl_sub = rospy.Subscriber('amcl_pose', PoseWithCovarianceStamped, r.set_current_pose)
 
-    # r.current_loc = [2, -3]
-    # print(what_node(TwoRoomsParameters.nodes_dict, r.current_loc))
-    #
-    # r.current_loc = [-2, -2]
-    # print(what_node(TwoRoomsParameters.nodes_dict, r.current_loc))
-    #
-    # r.current_loc = [2, 2]
-    # print(what_node(TwoRoomsParameters.nodes_dict, r.current_loc))
-
-
+        rospy.spin()
 
